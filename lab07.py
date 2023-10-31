ba07ed780c4dcc982beb863d693f25d7e0cf9249
Lhomas7 <landon7@clemson.edu>
2023-10-31 17:48:32 -0400

    ugkbj

--- lab07.py
+++ lab07.py
@@ -1,70 +1,167 @@
-class Toppings:
-    def __init__(self, user_toppings = []):
-        self.user_toppings = user_toppings
-    def cost(self, user_toppings):
-        cost = 0
-        for topping in self.user_toppings:
-            if self.topping == 'sprinkles':
-                cost += 0.15
-            elif self.topping == 'gummy bears':
-                cost += 0.45
-            elif self.topping == 'oreos':
-                cost +=0.38
-            else:
-                cost += 0
-        return cost
-    
 
 class Ice_cream:
-    def __init__(self, flavor, choice, num_scoops,toppings = Toppings()):
-        self.flavor = flavor
-        self.choice = choice
+    def __init__(self):
+        self.toppings = []
+        self.deluxe = False
+        self.num_scoops = 1
+        self.price = 0
+        self.flavor = ''
+    def get_flavor(self):
+        return self.flavor
+    def get_num_scoops(self):
+        return self.num_scoops
+    def get_choice(self):
+        return self.choice
+    def get_toppings(self):
+        return self.toppings
+    def set_flavor(self, flavor):
+        if flavor.lower() not in ['vanilla','chocolate','strawberry']:
+            raise ValueError("Please put in a valid ice cream flavor.")
+        self.flavor = flavor.lower()
+        if self.flavor == 'vanilla':
+            self.price = 1.05
+        elif self.flavor == 'chocolate':
+            self.price = 1.12
+        elif self.flavor == 'strawberry':
+            self.price = 1.32
+    def set_num_scoops(self, num_scoops):
+        if num_scoops <= 0:
+            raise ValueError("Please enter a number greater than 0")
         self.num_scoops = num_scoops
+        self.price *= num_scoops
+    def set_choice(self, deluxe):
+        self.deluxe = deluxe
+    def set_toppings(self,toppings):
         self.toppings = toppings
-    def cost(self):
-        cost = 0
-        if self.choice == 'yes':
-            if self.flavor == 'Vanilla':
-                cost += 1.491 * self.num_scoops
-            elif self.flavor == 'Chocolate':
-                cost += 1.5904 * self.num_scoops
-            else:
-                cost += 1.8744 * self.num_scoops
-        else:
-            if self.flavor == 'Vanilla':
-                cost += 1.05 * self.num_scoops
-            elif self.flavor == 'Chocolate':
-                cost += 1.12 * self.num_scoops
-            else:
-                cost += 1.32 * self.num_scoops
-        return cost
+    def calc_total(self):
+        total = self.price
+        if self.deluxe:
+            total *= 1.42
+        for topping in self.toppings:
+            total += topping.get_cost()
+        return total
     def ice_cream_info(self):
-        print(f"Flavor: {self.flavor}")
-        print(f"Scoops: {self.num_scoops}")
-        if self.choice == 'yes':
-            print(f"Deluxe: True")
-        else:
-            print(f"Deluxe: False")
-        if self.toppings.cost() == 0: 
-            print(f"Toppings: NONE")
-        else:
-            print(f"Toppings: ", end = '')
-            for i in range(len(self.toppings.user_toppings())):
-                print(f"{user_toppings[i]} ", end = '')
+        info = f"\nFlavor: {self.flavor}"
+        info += f"\nScoops: {self.num_scoops}"
+        info += f"\nDeluxe: {self.deluxe}"
+        info += f"\nToppings: "
+        for t in self.toppings:
+            info += t.get_type() + " "
+        if not self.toppings:
+            info += "NONE"
+        info += f"\nTotal: ${self.calc_total():.2f}\n"
+        return info
+
+
+class Topping:
+    def __init__(self,type):
+        self.type = type.lower()
+        if self.type.lower() == 'sprinkles':
+            self.cost = 0.15
+        elif self.type.lower() == 'gummy bears':
+            self.cost = 0.45
+        elif self.type.lower() == 'oreos':
+            self.cost = 0.38
+    def get_type(self):
+        return self.type
+    def get_cost(self):
+        return self.cost
+    
+
 
     
 
         
 
 class Receipt:
-    def __init__(self, user_ice_cream = Ice_cream()):
-        self.name = name
-        self.user_ice_cream = user_ice_cream
-    def print_receipt(self,):
-        print("Adkin's Scoop City Receipt")
-        print(f"Customer Name: {self.name}")
-        print(self.user_ice_cream.ice_cream_info())
-        print(f"Total: ")
+    def __init__(self):
+        self.ice_creams = []
+        self.name = ''
+    def add(self,ice_cream):
+        self.ice_creams.append(ice_cream)
+    def set_name(self, name):
+        self.name = name.lower()
+    def get_name(self):
+        return self.name
+    def calc_total(self):
+        total = 0
+        for i in self.ice_creams:
+            total += i.calc_total()
+        return total
+    def print_receipt(self):
+        str_total = ""
+        str_total += "\nAdkins' Scoop City Receipt"
+        str_total += f"\nCustomer name:"
+        for i in self.ice_creams:
+            str_total += (i.ice_cream_info())
+        str_total += (f"Final Total: ${self.calc_total():.2f}")
+        return str_total
+    def main():
+        rec = Receipt()
+        print("Welcome to Adkins' Scoop City!")
+        print("What is your name?")
+        user_name = input()
+        like_to_order = 'y'
+        while like_to_order == 'y':
+            ice_cream = Ice_cream()
+            try:
+                print("What flavor of ice cream would you like to order?")
+                print("Your options are: Vanilla, Strawberry, Chocolate.")
+                user_flavor = input().strip().lower()
+                ice_cream.set_flavor(user_flavor)
+            except ValueError:
+                print("Please put in a valid ice cream flavor.")
+            print("Would you like the deluxe brand? (Yes/No)")
+            deluxe_choice = input().strip().lower()
+            while deluxe_choice != 'yes' and deluxe_choice != 'no':
+                print("Please input Yes or No!")
+                deluxe_choice = input().strip().lower()
+            ice_cream.set_choice(deluxe_choice == "yes")
+            try: 
+                print("How many scoops would you like to order?")
+                user_scoops = int(input())
+                ice_cream.set_num_scoops(user_scoops)
+            except ValueError:
+                print("Please enter a number greater than 0")
+            print("Which toppings would you like? Enter done if you do not want any.")
+            print("Your options are: sprinkles, gummy bears, oreos.")
+            toppings_temps = []
+            while True:
+                topping_type = input().strip().lower()
+                if topping_type == "done":
+                    break
+                elif topping_type in ["sprinkles","gummy bears","oreos"]:
+                    topping = Topping(topping_type)
+                    toppings_temps.append(topping)
+                    print(f"Toppings {topping.get_type()} added for ${topping.get_cost():.2f}")
+                    print("Enter done if you are done selecting toppings, or enter another topping.")
+                else:
+                    print("Please put in a valid topping type.")
+            ice_cream.set_toppings(toppings_temps)
+            rec.add(ice_cream)
+            print('Your order so far:')
+            print(rec.print_receipt())
+            print("Would you like to order another ice cream? (Yes/No)")
+            new_order = input().strip().lower()
+            while new_order != 'yes' and new_order != 'no':
+                print("Please input Yes or No!")
+            if new_order == 'yes':
+                continue
+            elif new_order == 'no':
+                like_to_order = 'n'
+            print("Your order:")
+            print(rec.print_receipt())
+    
+if __name__ == "__main__":
+    Receipt.main()
+
+
+
+            
+
+
+
+    
         
         
             
@@ -74,56 +171,7 @@ class Receipt:
 
 
 
-print("Welcome to Adkins' Scoop City!")
-print("What is your name?")
-user_name = input()
-like_to_order = 'y'
-while like_to_order == 'y':
-    print("What flavor of ice cream would you like to order?")
-    print("Your options are: Vanilla, Strawberry, Chocolate.")
-    user_flavor = input().lower()
-    while user_flavor != 'vanilla' and user_flavor != 'strawberry' and user_flavor != 'chocolate':
-        print("Please put in a valid ice cream flavor.")
-        user_flavor = input().lower()
-    print("Would you like the deluxe brand? (Yes/No)")
-    deluxe_choice = input().lower()
-    while deluxe_choice != 'yes' and deluxe_choice != 'no':
-        print("Please input Yes or No!")
-        deluxe_choice = input().lower()
-    print("How many scoops would you like to order?")
-    user_scoops = int(input())
-    while user_scoops <= 0:
-        print("Please enter a number greater than 0")
-        user_scoops = int(input())
-    users_ice_cream = Ice_cream(user_name, user_flavor, deluxe_choice, user_scoops, )
-    print("Which toppings would you like? Enter done if you do not want any.")
-    print("Your options are: sprinkles, gummy bears, oreos.")
-    user_toppings_list = []
-    user_choice_toppings = input().lower()
-    while user_choice_toppings != 'sprinkles' and user_choice_toppings != 'gummy bears' and user_choice_toppings != 'oreos' and user_choice_toppings != 'done':
-        print("Please put in a valid topping type.")
-        user_choice_toppings = input().lower().split()
-    user_toppings_list.append(user_choice_toppings)
-    print(f"Topping {user_choice_toppings} added for {}")
-    done = 'n'
-    while done == 'n':
-        print("Enter done if you are done selecting toppings, or enter another topping.")
-        user_input = input().lower()
-        if user_input == 'done':
-            break
-        while user_input != 'sprinkles' and user_input != 'gummy bears' and user_input != 'oreos' and user_input != 'done':
-            print("Please put in a valid topping type.")
-            user_input = input().lower()
-        if user_input == 'done':
-            done = 'y'
-        else:
-            if user_input == 'sprinkles':
-                print(f"Topping {user_input} added for $0.15")
-            user_choice_toppings.append(user_input)
-    users_toppings = Toppings(user_name, user_choice_toppings)
-    print('Your order so far:')
-    
-        
 
-    print("Would you like to order another ice cream? (Yes/No)")
-    print("Please input Yes or No!")
+    
+    
+    
